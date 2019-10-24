f9f7ee4d1d212bacb4b4cb445dcb3d7a714472ca
Taran <taranontheweb@gmail.com>
2019-10-24 17:31:46 -0400

    Activity Controller Created

---
+++ AcmeActivityManagement/Controllers/ActivityController.cs
@@ -0,0 +1,28 @@
+﻿using AcmeActivityManagement.EntityFramework;
+using AcmeActivityManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcmeActivityManagement.Controllers
+{
+    public class ActivityController : ControllerBase
+    {
+        private readonly AcmeDbContext acmeDbContext;
+
+        public ActivityController(AcmeDbContext acmeDbContext)
+        {
+            this.acmeDbContext = acmeDbContext;
+        }
+
+        [HttpGet("/api/activities")]
+        public async Task<IEnumerable<Activity>> GetActivities()
+        {
+            var activities = await this.acmeDbContext.Activities.ToListAsync();
+            return activities;
+        }
+    }
+}
