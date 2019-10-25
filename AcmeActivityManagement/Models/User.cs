46b1188e945f50ede73a993b8a35242cb8f8793d
Taran <taranontheweb@gmail.com>
2019-10-25 12:40:22 -0400

    User Model Created

---
+++ AcmeActivityManagement/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcmeActivityManagement.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public Activity Activity { get; set; }
+        public string Comment { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
