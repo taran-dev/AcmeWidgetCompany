e8eaf0f04014da27e5397218a964fde17b129545
Taran <taranontheweb@gmail.com>
2019-10-25 13:41:18 -0400

    User Controller Created

---
+++ AcmeActivityManagement/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using AcmeActivityManagement.EntityFramework;
+using AcmeActivityManagement.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcmeActivityManagement.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        private readonly AcmeDbContext acmeDbContext;
+        private readonly IMapper mapper;
+
+        public UserController(AcmeDbContext acmeDbContext, IMapper mapper)
+        {
+            this.acmeDbContext = acmeDbContext;
+            this.mapper = mapper;
+        }
+
+        [HttpPost("/api/users")]
+        public IActionResult CreateUser(User user)
+        {
+            return Ok(user);
+        }
+
+    }
+}
